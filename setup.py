21b3e1eb4b8a474c7dac23a872c4a0adcc3a3f4b
Matthew Andres Moreno <m.more500@gmail.com>
2019-04-25 20:09:58 -0400

    Bump version: 0.0.1 → 0.1.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='fileshash',
-      version='0.0.1',
+      version='0.1.0',
       description='Module and command-line tool that wraps around hashlib and zlib to facilitate generating checksums / hashes of files.',
       long_description=long_description,
       long_description_content_type='text/x-rst',
